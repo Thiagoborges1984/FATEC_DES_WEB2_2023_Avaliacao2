e0a464bc69ce3e4cd269b96d27ff215e0305bf8e
Thiago_R_Borges <125098942+Thiagoborges1984@users.noreply.github.com>
2023-10-16 21:51:25 -0300

    Add files via upload

---
+++ avaliacao2/portarias/cadastro_placa.php
@@ -0,0 +1,53 @@
+<?php
+
+    require_once('header.php');
+    require_once('dados_banco.php');
+
+    $aluno = $_GET['aluno'];
+    $placa = $_GET['placa'];
+
+    if(strlen($aluno) > 10 ){
+        header("location: cadastro.php");
+    }
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO veiculos (aluno, placa) VALUES ('$aluno', '$placa')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+?>
+ 
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Portaria Fatec</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>
+            <?php echo $_SESSION["username"]; ?>
+            <br>
+        </h1>
+    </div>
+    <p>
+    Aluno: <b>
+        <?php echo $aluno; ?>
+    </b>cadastrado com sucesso.
+    <br><br>
+        <a href="principal.php" class="btn btn-primary">Voltar</a>
+    <br><br>
+    </p>
+</body>
+</html>
